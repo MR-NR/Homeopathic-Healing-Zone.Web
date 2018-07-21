7bbb25c8da2708f985a59080dd989b8dfc320234
RAO NISAR AHMED <MR-NR@users.noreply.github.com>
2018-07-21 07:49:22 -0700

    Initial Project Created

---
+++ HHZ/HHZ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HHZ.Startup))]
+namespace HHZ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
